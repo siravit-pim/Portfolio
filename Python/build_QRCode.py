86eafc7a34ea5552ef883167f1b2df60b7ff26d0
zkiddy <120651969+zkiddy@users.noreply.github.com>
2023-02-12 18:57:46 +0700

    Create build_QRCode.py

---
+++ Python/build_QRCode.py
@@ -0,0 +1,15 @@
+# pip install qrcode
+import qrcode
+
+link = input("")
+qr = qrcode.QRCode(version = 1, box_size = 10, border =2)
+qr.add_data(link)
+qr.make(fit = True)
+img = qr.make_image(fill_color = "black", back_color = "while")
+
+img.save("qrCode.png")
+
+# ----------------------------
+# check path
+import os
+os.getcwd()
