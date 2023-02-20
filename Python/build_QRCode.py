0e13fd9e603f32f26875f91c90f98637ec602916
Siravit Pimaksorn <120651969+siravit-pim@users.noreply.github.com>
2023-02-20 10:30:03 +0700

    Update build_QRCode.py

--- Python/build_QRCode.py
+++ Python/build_QRCode.py
@@ -5,11 +5,11 @@ link = input("")
 qr = qrcode.QRCode(version = 1, box_size = 10, border =2)
 qr.add_data(link)
 qr.make(fit = True)
-img = qr.make_image(fill_color = "black", back_color = "while")
+img = qr.make_image(fill_color = "black", back_color = "white")
 
 img.save("qrCode.png")
 
 # ----------------------------
 # check path
-import os
-os.getcwd()
+#import os
+#os.getcwd()
