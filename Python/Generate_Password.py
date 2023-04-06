c3b089a52942e48483ace0588d42141b73e8f60b
Siravit Pimaksorn <120651969+siravit-pim@users.noreply.github.com>
2023-04-06 07:47:22 +0700

    Update Generate_Password.py

--- Python/Generate_Password.py
+++ Python/Generate_Password.py
@@ -74,3 +74,64 @@ while(True) :
     checker = int(input("wanna geralate again: [y/n]"))
     if checker == 1 :
         break
+#---------------------------------------------------------
+#---------------------------------------------------------
+#---------------------------------------------------------
+# With Tkinter
+import random
+import string
+import tkinter as tk
+import pyperclip as pc
+
+def generate():
+    entry.delete(0, tk.END)
+    length = lengthInput.get()
+    
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    num = string.digits
+    symbol = string.punctuation
+    
+    lowC = lower+upper
+    mediumC = lower+upper+num
+    strongC = lower+upper+num+symbol
+    password = ""
+    
+    if strengthInput.get() == 0:
+        for i in range(length):
+            password = password + random.choice(lowC)
+    elif strengthInput.get() == 1:
+         for i in range(length):
+            password = password + random.choice(mediumC)
+    elif strengthInput.get() == 2:
+         for i in range(length):
+            password = password + random.choice(strongC)  
+    entry.insert(0, password)
+    
+def copy():
+    randomPassword = entry.get()
+    pc.copy(randomPassword)
+
+root = tk.Tk()
+lengthInput = tk.IntVar()
+strengthInput = tk.IntVar()
+
+root.title("Random Password Generator")
+
+lengthLabel = tk.Label(root, text = "Preferred Lenght").grid(row=0, column=0)
+lengthBox = tk.Spinbox(root, from_ = 4, to = 10, textvariable = lengthInput).grid(row=0, column=1)
+
+strengthLabel = tk.Label(root, text = "Preferred Strength").grid(row=0, column=2)
+strengthLow = tk.Radiobutton(root, text = "Low", variable = strengthInput, value = 0).grid(row=0, column=3)
+strengthMedium = tk.Radiobutton(root, text = "Medium", variable = strengthInput, value = 1).grid(row=0, column=4)
+strengthStrong = tk.Radiobutton(root, text = "Strong", variable = strengthInput, value = 2).grid(row=0, column=5)
+
+randomPassword = tk.Label(root, text = "Password").grid(row=1, column=0)
+entry = tk.Entry(root)
+entry.grid(row=1, column=1)
+
+generateBotton = tk.Button(root, text = "Generate", command = generate).grid(row=1, column=2)
+copyBotton = tk.Button(root, text = "Copy",command = copy).grid(row=1, column=3)
+
+root.mainloop()
+quit()
