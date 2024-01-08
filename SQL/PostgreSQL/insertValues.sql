fb561817940478302b0e298ef92c2c84fa58273b
zKiddy <120651969+siravit-pim@users.noreply.github.com>
2024-01-08 16:19:57 +0700

    Update and rename insert.sql to insertValues.sql

--- SQL/PostgreSQL/insertValues.sql
+++ SQL/PostgreSQL/insertValues.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 -- Inserts value for 'post_harvest_storage' table
-INSERT INTO ucessuv.post_harvest_storage (post_harvest_storage_id, post_harvest_storage_name, post_harvest_storage_type, built_date, location)
+INSERT INTO xxxxx.post_harvest_storage (post_harvest_storage_id, post_harvest_storage_name, post_harvest_storage_type, built_date, location)
 VALUES 
     (1, 'a', 'room_temperature_storage', '2023-01-01', ST_Extrude(ST_GeomFromText('POLYGON ((384150 478450 0, 384450 478450 0, 384600 478750 0, 384300 478750 0, 384150 478450 0))', 27700), 0, 0, 150)),
     (2, 'b', 'cool_storage', '2023-12-31', ST_Extrude(ST_GeomFromText('POLYGON ((384650 478450 0, 384950 478450 0, 385100 478750 0, 384800 478750 0, 384650 478450 0))', 27700), 0, 0, 150));
@@ -9,13 +9,13 @@ VALUES
 --=============================
 
 -- Inserts for 'seed_storage' table
-INSERT INTO ucessuv.seed_storage (seed_storage_id, post_harvest_storage_id, seed_storage_name, seed_storage_type, location)
+INSERT INTO xxxxx.seed_storage (seed_storage_id, post_harvest_storage_id, seed_storage_name, seed_storage_type, location)
 VALUES
     (
         1,
         (
             SELECT post_harvest_storage_id 
-            FROM ucessuv.post_harvest_storage b 
+            FROM xxxxx.post_harvest_storage b 
             WHERE ST_3Dintersects(b.location, 
             ST_GeomFromText('POLYGON ((384650 478450 0, 384950 478450 0, 385100 478750 0, 384800 478750 0, 384650 478450 0))', 27700))
         ),
@@ -26,7 +26,7 @@ VALUES
         2,
         (
             SELECT post_harvest_storage_id 
-            FROM ucessuv.post_harvest_storage b 
+            FROM xxxxx.post_harvest_storage b 
             WHERE ST_3Dintersects(b.location, 
             ST_GeomFromText('POLYGON ((384150 478450 0, 384450 478450 0, 384600 478750 0, 384300 478750 0, 384150 478450 0))', 27700))
         ),
@@ -37,7 +37,7 @@ VALUES
         3,
         (
             SELECT post_harvest_storage_id 
-            FROM ucessuv.post_harvest_storage b 
+            FROM xxxxx.post_harvest_storage b 
             WHERE ST_3Dintersects(b.location, 
             ST_GeomFromText('POLYGON ((384650 478450 0, 384950 478450 0, 385100 478750 0, 384800 478750 0, 384650 478450 0))', 27700))
         ),
@@ -51,14 +51,14 @@ VALUES
 -- Inserts for 'seed_stations' table
 
 -- Inserts for 'seed_stations' table
-INSERT INTO ucessuv.seed_stations
+INSERT INTO xxxxx.seed_stations
     (seed_station_id, seed_storage_id, location)
 VALUES
     (
         1,
         (
             SELECT seed_storage_id 
-            FROM ucessuv.seed_storage b 
+            FROM xxxxx.seed_storage b 
             WHERE ST_3Dintersects(b.location, ST_GeomFromText('POLYGON((384650 478450 0, 384800 478450 0, 384950 478750 0, 384800 478750 0, 384650 478450 0))', 27700)) 
             LIMIT 1
         ),
@@ -68,7 +68,7 @@ VALUES
         2,
         (
             SELECT seed_storage_id 
-            FROM ucessuv.seed_storage b 
+            FROM xxxxx.seed_storage b 
             WHERE ST_3Dintersects(b.location, ST_GeomFromText('POLYGON((384650 478450 0, 384800 478450 0, 384950 478750 0, 384800 478750 0, 384650 478450 0))', 27700)) 
             LIMIT 1
         ),
@@ -78,7 +78,7 @@ VALUES
         3,
         (
             SELECT seed_storage_id 
-            FROM ucessuv.seed_storage b 
+            FROM xxxxx.seed_storage b 
             WHERE ST_3Dintersects(b.location, ST_GeomFromText('POLYGON ((384150 478450 0, 384300 478450 0, 384450 478750 0, 384300 478750 0, 384150 478450 0))', 27700))
         ),
         ST_GeomFromText('POINT (384200 478500)', 27700)
@@ -87,7 +87,7 @@ VALUES
         4,
         (
             SELECT seed_storage_id 
-            FROM ucessuv.seed_storage b 
+            FROM xxxxx.seed_storage b 
             WHERE ST_3Dintersects(b.location, ST_GeomFromText('POLYGON ((384150 478450 0, 384300 478450 0, 384450 478750 0, 384300 478750 0, 384150 478450 0))', 27700))
         ),
         ST_GeomFromText('POINT (384250 478600)', 27700)
@@ -96,7 +96,7 @@ VALUES
         5,
         (
             SELECT seed_storage_id 
-            FROM ucessuv.seed_storage b 
+            FROM xxxxx.seed_storage b 
             WHERE ST_3Dintersects(b.location, ST_GeomFromText('POLYGON ((384150 478450 0, 384300 478450 0, 384450 478750 0, 384300 478750 0, 384150 478450 0))', 27700))
         ),
         ST_GeomFromText('POINT (384275 478550)', 27700)
@@ -105,7 +105,7 @@ VALUES
         6,
         (
             SELECT seed_storage_id 
-            FROM ucessuv.seed_storage b 
+            FROM xxxxx.seed_storage b 
             WHERE ST_3Dintersects(b.location, ST_GeomFromText('POLYGON ((384800 478450 0, 384950 478450 0, 385100 478750 0, 384950 478750 0, 384800 478450 0))', 27700)) 
             ORDER BY 1 DESC LIMIT 1
         ),
@@ -115,7 +115,7 @@ VALUES
         7,
         (
             SELECT seed_storage_id 
-            FROM ucessuv.seed_storage b 
+            FROM xxxxx.seed_storage b 
             WHERE ST_3Dintersects(b.location, ST_GeomFromText('POLYGON ((384800 478450 0, 384950 478450 0, 385100 478750 0, 384950 478750 0, 384800 478450 0))', 27700)) 
             ORDER BY 1 DESC LIMIT 1
         ),
@@ -125,7 +125,7 @@ VALUES
         8,
         (
             SELECT seed_storage_id 
-            FROM ucessuv.seed_storage b 
+            FROM xxxxx.seed_storage b 
             WHERE ST_3Dintersects(b.location, ST_GeomFromText('POLYGON ((384800 478450 0, 384950 478450 0, 385100 478750 0, 384950 478750 0, 384800 478450 0))', 27700)) 
             ORDER BY 1 DESC LIMIT 1
         ),
@@ -135,7 +135,7 @@ VALUES
         9,
         (
             SELECT seed_storage_id 
-            FROM ucessuv.seed_storage b 
+            FROM xxxxx.seed_storage b 
             WHERE ST_3Dintersects(b.location, ST_GeomFromText('POLYGON ((384800 478450 0, 384950 478450 0, 385100 478750 0, 384950 478750 0, 384800 478450 0))', 27700)) 
             ORDER BY 1 DESC LIMIT 1
         ),
@@ -146,7 +146,7 @@ VALUES
 --=============================
 
 -- Inserts for 'testers' table
-INSERT INTO ucessuv.testers (tester_id, tester_name)
+INSERT INTO xxxxx.testers (tester_id, tester_name)
 VALUES 
     (1, 'Peter'),
     (2, 'Alice'),
@@ -158,7 +158,7 @@ VALUES
 --=============================
 
 -- Inserts for 'condition_indicator' table
-INSERT INTO ucessuv.condition_indicator (condition_indicator_id, condition_indicator_description)
+INSERT INTO xxxxx.condition_indicator (condition_indicator_id, condition_indicator_description)
 VALUES 
     (1, 'most_suitable'),
     (2, 'suitable'),
@@ -167,7 +167,7 @@ VALUES
 --=============================
 
 --seed_storage_condition
-INSERT INTO ucessuv.seed_storage_condition (seed_storage_condition_id, seed_storage_id, temperature_condition, tester_id, report_date)
+INSERT INTO xxxxx.seed_storage_condition (seed_storage_condition_id, seed_storage_id, temperature_condition, tester_id, report_date)
 VALUES 
     (1, 1, 3, 1, '2023-05-15'),
     (2, 2, 2, 2, '2023-05-01'),
@@ -179,7 +179,7 @@ VALUES
 --=============================
 
 -- Inserts for 'seed_stations_condition' table
-INSERT INTO ucessuv.seed_stations_condition (seed_station_id, size_condition, moisture_condition, genetic_purity_condition, tester_id, report_date)
+INSERT INTO xxxxx.seed_stations_condition (seed_station_id, size_condition, moisture_condition, genetic_purity_condition, tester_id, report_date)
 VALUES 
     (1, 1, 1, 1, 1, '2023-08-31'),
     (2, 1, 2, 2, 2, '2023-08-15'),
@@ -194,7 +194,7 @@ VALUES
 --=============================	
 
 -- Inserts for 'parameters' table
-INSERT INTO ucessuv.parameters
+INSERT INTO xxxxx.parameters
 (parameter_id, parameter_type, parameter_name, parameter_subname, parameter_value, parameter_units)
 VALUES
 	(101, 'cost', 'post_harvest_storage', 'room_temperature_storage', 1000, '£ per sq m'),
